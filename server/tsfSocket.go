1cf1737b6cbf6dc3060e7920357839b65139f268
donnie <donnie4w@gmail.com>
2023-10-09 16:59:36 +0800

    Create tsfSocket.go

---
+++ server/tsfSocket.go
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2023 tsf Author. All Rights Reserved.
+ * email: donnie4w@gmail.com
+ */
+
+package server
+
+import "sync"
+
+type TsfSocket interface {
+	_Incount () int64
+	_SubAndGet() int64
+	_Mux() *sync.Mutex
+	_DataChan() chan []byte
+	IsValid() bool
+	Cfg() *TConfiguration
+
+	Write([]byte) (int, error)
+	Read([]byte) (int, error)
+	
+	WriteWithLen(buf []byte) (int, error)
+	WriteWithMerge(buf []byte) (i int, err error)
+
+	Interrupt() error 
+	Close() error
+}
