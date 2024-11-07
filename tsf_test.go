bbad7bef3bf17d66641bc487e9375ede1fe2048d
donnie4w <donnie4w@gmail.com>
2024-11-07 16:19:06 +0800

    Update tsf_test.go

--- tsf_test.go
+++ tsf_test.go
@@ -19,30 +19,76 @@ package tsf
 
 import (
 	"fmt"
+	"sync/atomic"
 	"testing"
+	"time"
 )
 
 func Test_tsf(t *testing.T) {
-	cfg := &TsfConfig{ListenAddr: ":20001"}
-	cfg.OnOpen = func(socket TsfSocket) error {
+	cfg := &TsfConfig{ListenAddr: ":20001", TConfiguration: &TConfiguration{ProcessMerge: true}}
+	tx := &TContext{}
+	tx.OnOpen = func(socket TsfSocket) error {
 		fmt.Println("OnOpen:", socket.ID())
 		return nil
 	}
-	cfg.Handler = func(socket TsfSocket, bytes []byte) error {
-		fmt.Println(string(bytes))
+	tx.Handler = func(socket TsfSocket, packet *Packet) error {
+		fmt.Println("server recv:", string(packet.ToBytes()))
+		if _, err := socket.WriteWithMerge(packet.ToBytes()); err != nil {
+			panic(err)
+		}
 		return nil
 	}
-	cfg.OnClose = func(socket TsfSocket) error {
+	tx.OnClose = func(socket TsfSocket) error {
 		fmt.Println("OnClose:", socket.ID())
 		return nil
 	}
-	cfg.OnError = func(err error, socket TsfSocket) error {
+	tx.OnError = func(err error, socket TsfSocket) error {
 		fmt.Println("OnError:", socket.ID(), ",err:", err)
 		return nil
 	}
-	s, err := NewTsf(cfg)
+	s, err := NewTsf(cfg, tx)
 	if err != nil {
 		t.Fatal(err)
 	}
 	s.Serve()
+}
+
+func Test_tsfclient(t *testing.T) {
+	tx := &TContext{}
+	var i atomic.Int32
+	tx.OnOpen = func(socket TsfSocket) error {
+		fmt.Println("OnOpen:", socket.ID())
+		if _, err := socket.WriteWithMerge([]byte(fmt.Sprint(i.Add(1)))); err != nil {
+			panic(err)
+		}
+		return nil
+	}
+	tx.Handler = func(socket TsfSocket, packet *Packet) error {
+		fmt.Println("client recv:", string(packet.ToBytes()))
+		if i.Load() < 10 {
+			for range 20 {
+				if _, err := socket.WriteWithMerge([]byte(fmt.Sprint(i.Add(1)))); err != nil {
+					panic(err)
+				}
+			}
+
+		}
+		return nil
+	}
+	tx.OnClose = func(socket TsfSocket) error {
+		fmt.Println("OnClose:", socket.ID())
+		return nil
+	}
+	tx.OnError = func(err error, socket TsfSocket) error {
+		fmt.Println("OnError:", socket.ID(), ",err:", err)
+		return nil
+	}
+
+	sock := NewTSocketConf(":20001", &TConfiguration{ConnectTimeout: 10 * time.Second, ProcessMerge: true})
+
+	if err := sock.Open(); err == nil {
+		sock.On(tx)
+	} else {
+		panic(err.Error())
+	}
 }
